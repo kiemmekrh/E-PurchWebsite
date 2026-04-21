a69c8376b53a92062a276566d6fe3e5f7fe7a05c
Kiemme Sarah Foutoula Koroh <kiemmekoroh@gmail.com>
2026-04-21 22:40:47 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Purch</title>
+    <title>E-Purch - Login | PT. NIRAMAS UTAMA</title>
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
 <body>
